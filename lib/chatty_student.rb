7a57dffd4af5c656b96888db751151358dcfbd80
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-09-11 19:41:46 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,6 +1,6 @@
 class Chatty_student 
   
-  def hello
-    super
-    "Hey there! I'm so excited to learn stuff."
+  def hello 
+    super 
+    "How are you today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
 end
