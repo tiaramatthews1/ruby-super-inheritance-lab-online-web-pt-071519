d4aeaf3170211ee0492debdc7afb08459121cfd1
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-09-11 19:42:03 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -4,3 +4,6 @@ class Chatty_student
     super 
     "How are you today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
 end
+
+def raise_hand
+  su
